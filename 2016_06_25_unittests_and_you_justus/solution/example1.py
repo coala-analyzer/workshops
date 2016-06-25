eba711093ce8660d31ccd2020065482aef9a999c
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-06-25 19:09:22 +0200

    implement full function

--- 2016_06_25_unittests_and_you_justus/solution/example1.py
+++ 2016_06_25_unittests_and_you_justus/solution/example1.py
@@ -1,10 +1,22 @@
+from string import punctuation
+
 def reverse_word(word):
     """Reverse the words in a string."""
     return "".join(rev.upper() if letter.isupper() else rev.lower()
                    for rev, letter in zip(reversed(word), word))
 
 def reverse_words(sentence):
-    return " ".join(reverse_word(word) for word in sentence.split())
+    result = ''
+    word = ''
+    for character in sentence:
+        if character in punctuation + ' ':
+            result +=  reverse_word(word) + character
+            word = ''
+        else:
+            word += character
+
+    result += reverse_word(word)
+    return result
 
 from unittest import TestCase
 
@@ -19,9 +31,12 @@ class ReverseStringTestCase(TestCase):
     def test_cased_word(self):
         self.assertEqual(reverse_words("hEllo"), "oLleh")
 
+    def test_punctuation(self):
+        self.assertEqual(reverse_words("hEllo,"), "oLleh,")
+
     def test_simple_sentence(self):
         self.assertEqual(reverse_words("hello world"), "olleh dlrow")
 
     def test_sentence(self):
         self.assertEqual(reverse_words("Hello, world - you're awesome!"),
-                         "Olleh, dlrow - er'uoy emosewa!")
+                         "Olleh, dlrow - uoy'er emosewa!")
