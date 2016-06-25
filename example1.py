43b3be3cf3b7d0cdf481f06469e5971c795dc1ad
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-06-25 18:22:25 +0200

    Initial Commit

---
+++ example1.py
@@ -0,0 +1,16 @@
+def reverse_words(string):
+    """Reverse the words in a string."""
+    pass  # TODO
+
+from unittest import TestCase
+
+
+class ReverseStringTestCase(TestCase):
+    def test_empty_string(self):
+        pass  # TODO
+
+    def test_single_word(self):
+        pass  # TODO
+
+    def test_sentence(self):
+        pass # TODO
---
+++ example2.py
@@ -0,0 +1,28 @@
+"""."""
+from unittest import TestCase
+
+
+class Account:
+    """An account stores money."""
+
+    balance = 0
+
+    transactions = []  # TODO
+
+    def deposit(self, amount):
+        """Deposit money on this account."""
+
+    def withdraw(self, amount):
+        """Withdraw money from this account."""
+
+    def current_balance(self):
+        """Report current balance."""
+
+
+class AccountTestCase(TestCase):
+    """."""
+
+    def test_deposit(self):
+        pass  # TODO
+
+def test_withdraw(self):
