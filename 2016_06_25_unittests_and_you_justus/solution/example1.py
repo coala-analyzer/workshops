9b6b1ca717fb50e0be53b96ea116fc9d1dd377c5
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-06-25 18:55:21 +0200

    example: Make it handle uppers well

--- 2016_06_25_unittests_and_you_justus/solution/example1.py
+++ 2016_06_25_unittests_and_you_justus/solution/example1.py
@@ -1,6 +1,10 @@
-def reverse_words(string):
+def reverse_word(word):
     """Reverse the words in a string."""
-    pass  # TODO
+    return "".join(rev.upper() if letter.isupper() else rev.lower()
+                   for rev, letter in zip(reversed(word), word))
+
+def reverse_words(sentence):
+    return " ".join(reverse_word(word) for word in sentence.split())
 
 from unittest import TestCase
 
@@ -12,9 +16,12 @@ class ReverseStringTestCase(TestCase):
     def test_single_word(self):
         self.assertEqual(reverse_words("hello"), "olleh")
 
+    def test_cased_word(self):
+        self.assertEqual(reverse_words("hEllo"), "oLleh")
+
     def test_simple_sentence(self):
         self.assertEqual(reverse_words("hello world"), "olleh dlrow")
 
     def test_sentence(self):
-        self.assertEqual(reverse_words("Hello world - you're awesome!"),
-                         "olleH dlrow - er'uoy emosewa!")
+        self.assertEqual(reverse_words("Hello, world - you're awesome!"),
+                         "Olleh, dlrow - er'uoy emosewa!")
